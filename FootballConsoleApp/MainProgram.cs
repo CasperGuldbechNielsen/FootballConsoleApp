6807e3d5b6690c9428466074f99920d17e3f9fb9
Casper Nielsen <Casp674d@edu.easj.dk>
2016-09-28 12:56:50 +0200

    Added menu

--- FootballConsoleApp/MainProgram.cs
+++ FootballConsoleApp/MainProgram.cs
@@ -1,11 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace FootballConsoleApp
 {
     public class MainProgram
     {
-        public MainProgram(int x)
+        public MainProgram()
         {
+            int x = ShowMenu();
+
             switch (x)
             {
                 case 1:
@@ -18,6 +21,20 @@ namespace FootballConsoleApp
                     break;
 
             }
-        } 
+        }
+
+        private int ShowMenu()
+        {
+            Console.WriteLine("Please select what you want to do:");
+            Console.WriteLine("1. Show league standing \n" +
+                              "2. Show news \n" +
+                              "3. Show upcomming matches");
+
+            string input = Console.ReadLine();
+
+            int output = Int32.Parse(input);
+
+            return output;
+        }
     }
 }
--- FootballConsoleApp/Program.cs
+++ FootballConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace FootballConsoleApp
     {
         static void Main(string[] args)
         {
-
+            MainProgram main = new MainProgram();
         }
     }
 }
