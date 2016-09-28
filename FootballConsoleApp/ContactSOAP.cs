3beb17400c60deefa87e589f03d4ca8da431ed78
Casper Nielsen <Casp674d@edu.easj.dk>
2016-09-28 13:04:24 +0200

    Added Contact class

---
+++ FootballConsoleApp/ContactSOAP.cs
@@ -0,0 +1,19 @@
+﻿using FootballConsoleApp.ServiceReference1;
+
+namespace FootballConsoleApp
+{
+    public class ContactSOAP
+    {
+        private ServiceReference1.SportsdataSoapClient client;
+
+        public ContactSOAP()
+        {
+            client = new SportsdataSoapClient();
+        }
+
+        public void GetLeageStanding()
+        {
+            //TODO: Stuff
+        }
+    }
+}
